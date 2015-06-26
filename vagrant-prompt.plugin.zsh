275b1fba4dab46a85c72fa5bb03f399248e53c7b
proud <alberto.re@gmail.com>
2015-06-26 10:11:59 +0200

    Handling also `aborted` status

--- vagrant-prompt.plugin.zsh
+++ vagrant-prompt.plugin.zsh
@@ -1,20 +1,18 @@
-# vim:ft=zsh ts=2 sw=2 sts=2
-#
 # To display Vagrant infos on your prompt add the vagrant_prompt_info to the
 # $PROMPT variable in your theme. Example:
 #
 # PROMPT='%{$fg[$NCOLOR]%}%B%n%b%{$reset_color%}:%{$fg[blue]%}%B%c/%b%{$reset_color%} $(vagrant_prompt_info)$(svn_prompt_info)$(git_prompt_info)%(!.#.$) '
 #
-# `vagrant_prompt_info` makes use of some custom variables. This is an example
-# definition:
+# `vagrant_prompt_info` makes use of some custom variables. This is the list
+# with some example values:
 #
 # ZSH_THEME_VAGRANT_PROMPT_PREFIX="%{$fg_bold[blue]%}["
 # ZSH_THEME_VAGRANT_PROMPT_SUFFIX="%{$fg_bold[blue]%}]%{$reset_color%} "
 # ZSH_THEME_VAGRANT_PROMPT_RUNNING="%{$fg_no_bold[green]%}●"
 # ZSH_THEME_VAGRANT_PROMPT_POWEROFF="%{$fg_no_bold[red]%}●"
 # ZSH_THEME_VAGRANT_PROMPT_SUSPENDED="%{$fg_no_bold[yellow]%}●"
+# ZSH_THEME_VAGRANT_PROMPT_ABORTED="%{$fg_no_bold[purple]%}●"
 # ZSH_THEME_VAGRANT_PROMPT_NOT_CREATED="%{$fg_no_bold[white]%}○"
-
 function vagrant_prompt_info() {
   test -d .vagrant && test -f Vagrantfile
   if [[ "$?" == "0" ]]; then
@@ -22,13 +20,15 @@ function vagrant_prompt_info() {
     statuses=("${(f)statuses}")
     printf '%s' $ZSH_THEME_VAGRANT_PROMPT_PREFIX
     for vm_details in $statuses; do
-      vm_state=$(echo $vm_details | grep -o -E "saved|poweroff|not created|running")
+      vm_state=$(echo $vm_details | grep -o -E "saved|poweroff|not created|running|aborted")
       if [[ "$vm_state" == "running" ]]; then
         printf '%s' $ZSH_THEME_VAGRANT_PROMPT_RUNNING
       elif [[ "$vm_state" == "saved" ]]; then
         printf '%s' $ZSH_THEME_VAGRANT_PROMPT_SUSPENDED
       elif [[ "$vm_state" == "not created" ]]; then
         printf '%s' $ZSH_THEME_VAGRANT_PROMPT_NOT_CREATED
+      elif [[ "$vm_state" == "aborted" ]]; then
+        printf '%s' $ZSH_THEME_VAGRANT_PROMPT_ABORTED
       elif [[ "$vm_state" == "poweroff" ]]; then
         printf '%s' $ZSH_THEME_VAGRANT_PROMPT_POWEROFF
       fi
